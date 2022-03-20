ed6ebab83bfd74526591a9c8578116fa5f822afe
Ivan <mikov_ivan@list.ru>
2022-03-20 11:00:49 +0300

    add nav drawer

--- app/src/main/java/ru/mikov/habittracker/ui/MainActivity.kt
+++ app/src/main/java/ru/mikov/habittracker/ui/MainActivity.kt
@@ -2,7 +2,13 @@ package ru.mikov.habittracker.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import ru.mikov.habittracker.R
 import ru.mikov.habittracker.databinding.ActivityMainBinding
@@ -10,6 +16,8 @@ import ru.mikov.habittracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val viewBinding: ActivityMainBinding by viewBinding()
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +25,15 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph, viewBinding.drawerLayout)
+
+        viewBinding.navView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
